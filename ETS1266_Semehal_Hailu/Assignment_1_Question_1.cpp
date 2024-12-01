151bd540c90a0db3c6bf78392b3b09e34d4a7063
Semehal Hailu Woldeslassie <semehal.hailu@aastustudent.edu.et>
2024-12-02 01:38:54 +0300

    Update Assignment_1_Question_1.cpp

--- ETS1266_Semehal_Hailu/Assignment_1_Question_1.cpp
+++ ETS1266_Semehal_Hailu/Assignment_1_Question_1.cpp
@@ -1,6 +1,6 @@
 Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
 in normal weight, under weight or over weight. BMI is calculated as (BMI = w/h*h). Make the program to
-work for multiple person before terminated ?
+work for multiple person before terminated.
 
 
 #include <iostream>
