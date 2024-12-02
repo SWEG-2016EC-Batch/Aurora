c04a5ee76dce314321fb27b3ca68796839950199
Sifen tesfaye kumsa <sifen.tesfaye@aastustudent.edu.et>
2024-12-02 03:24:51 +0300

    BMI Calculeter.Main.cpp

--- ETS1282 _Sifen_Tesfaye/Main.cpp
+++ ETS1282 _Sifen_Tesfaye/Main.cpp
@@ -1,42 +1,47 @@
-Step-by-Step pseudocod:
-__Start
-__Begin the program.
-__Input Weight and Height
+/*Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
+in normal weight, under weight or over weight. BMI is calculated as (BMI = w/h*h). Make the program to
+work for multiple person .*/
 
-Prompt user to enter their weight.
-Read the weight.
-Prompt user to enter their height.
-Read the height.
 
-__Calculate BMI
-__Calculate BMI using the formula: BMI = weight / (height * height).
-__Display BMI
+#include<iostream>
+using namespace std;
+int main(){
+    float weight,height,BMI,choice;
+    do
+    {
+     cout<<"Please enter your weight : "<<endl;
+    cin>>weight;
+    cout<<"Please enter your height : "<<endl;
+    cin>>height;
+    BMI=weight/ (height* height);
+    cout<<"BMI = "<<BMI<<endl;
+      if (BMI<18.5)
+      {
+        cout<<"underweight"<<endl;
+    }else if (BMI>18.5 && BMI<24.9)
+    {
+        cout<<"normal weight"<<endl;
+    }else if (BMI<25 && BMI >29.9)
+    {
+        cout<<"overweight"<<endl;
+    }else{
+        cout<<"obese"<<endl;
+    } 
+    cout<<"Do you want to continue if so press 1 if not press 0"<<endl;
+    cin>>choice;
+    }
+    while (choice=='0');
+    cout<<"we finish thank you";
+    
+    
 
-Print the calculated BMI.e
-Determine Weight Category
-IF BMI < 18.5:
-Print "Underweight".
-ELSE IF BMI >= 18.5 AND BMI <= 24.9:
-Print "Normal weight".
-ELSE IF BMI >= 25 AND BMI <= 29.9:
-Print "Overweight".
-ELSE:
-Print "Obese".
-
-__Prompt to Continue
-
-Ask if the user wants to calculate BMI for another person (1 to continue, 0 to terminate).
-Read user's choice.
-Decision
-IF choice == 0:
-Print "We finish, thank you".
-End the program.
-ELSE:
-Return to step 2.
-
-__End
-End the program
-
+    
+    
+    
+    
+    
+    return 0;
+}
 
 
 
