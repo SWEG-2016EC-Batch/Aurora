d723adf24c8abca312873c872afac3ed08ca0daf
Sifen tesfaye kumsa <sifen.tesfaye@aastustudent.edu.et>
2024-12-01 22:04:38 -0800

    Update  transmission.cpp

--- ETS1282 _Sifen_Tesfaye/ transmission.cpp
+++ ETS1282 _Sifen_Tesfaye/ transmission.cpp
@@ -1,29 +1,33 @@
-/*A serial transmission line can transmit 960 characters a second. Write a program that will calculate how long it will take to send a file, given the file size.
-  Test your program on a 400MB (419,430,400 byte) file which may take days.*/
-#include<iostream>
+/*A serial transmission line can transmit 960 characters a second. Write a program that will calculate how long
+it will take to send a file, given the file size. Test your program on a 400MB (419,430,400 byte) file which may
+take days.*/
+#include <iostream>
 using namespace std;
+int main(){
+double size,second,time_in_second ,time_in_hour, time_in_day;
+cout<<"enter the size of file you want to transmit in mega byte "<<endl;
+cin>>size;
+    /* 690 character in second means one character have 1Byte so we can transfer 690 bite per second. 
+    when we convert in to mega bite we divide by 1,000,000  it become 0.00096MB per second. */
+    time_in_second=size/0.00096;
+   /* 1 second =0.00096MB
+    required time=size given ,
+        to get the required time =size given*1 second/0.00096MB*/
+time_in_hour=time_in_second/3600;
+    //to convert in to hour we divide by 3600 because 1second =3600hour
+time_in_day=time_in_hour/24;
+    //to covert in a day we divide by 24 because 1day=24 hour
+cout<<"the time to take transmit "<<size<<"MB "<<endl;
+cout<<"in second  is  "<<time_in_second<<" seconds"<<endl;
+cout<<"in hour  is  "<<time_in_hour<<" hours"<<endl;
+cout<<"in day  is  "<<time_in_day<<" days"<<endl;
 
-int main() {
-    double size_in_MB;
-    double time_in_seconds, time_in_hours, time_in_days;
-
-    cout << "Please enter the size of the file you want to transmit in megabytes: " << endl;
-    cin >> size_in_MB;
-
-    double size_in_bytes = size_in_MB * 1048576; 
-  // Convert MB to bytes
-    time_in_seconds = size_in_bytes / 960;     
-  // Calculate time in seconds
-    time_in_hours = time_in_seconds / 3600;   
-  // Convert time to hours
-    time_in_days = time_in_hours / 24;         
-  // Convert time to days
-
-    cout << "The time it takes to transmit " << size_in_MB << " megabytes is: " << endl;
-    cout << "Time in seconds = " << time_in_seconds << endl;
-    cout << "Time in hours = " << time_in_hours << endl;
-    cout << "Time in days = " << time_in_days << endl;
-
+   /* then when we check for 400MB we get
+    400
+the time to take transmit 400MB is
+in second416667 seconds
+in hour115.741 hours
+in day4.82253 days */
+    
     return 0;
 }
-
