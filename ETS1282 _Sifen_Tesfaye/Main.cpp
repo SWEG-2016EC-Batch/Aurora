6c85dac1f501cceca9ffad05470fd7d58a8aa6c4
Sifen tesfaye kumsa <sifen.tesfaye@aastustudent.edu.et>
2024-12-01 16:01:13 -0800

    algorithm for BMI Main.cpp

--- ETS1282 _Sifen_Tesfaye/Main.cpp
+++ ETS1282 _Sifen_Tesfaye/Main.cpp
@@ -1,6 +1,45 @@
-#include <iostream >
-using namespace std;
-int main(){
+Step-by-Step pseudocod:
+__Start
+__Begin the program.
+__Input Weight and Height
 
-return 0;
-}
+Prompt user to enter their weight.
+Read the weight.
+Prompt user to enter their height.
+Read the height.
+
+__Calculate BMI
+__Calculate BMI using the formula: BMI = weight / (height * height).
+__Display BMI
+
+Print the calculated BMI.e
+Determine Weight Category
+IF BMI < 18.5:
+Print "Underweight".
+ELSE IF BMI >= 18.5 AND BMI <= 24.9:
+Print "Normal weight".
+ELSE IF BMI >= 25 AND BMI <= 29.9:
+Print "Overweight".
+ELSE:
+Print "Obese".
+
+__Prompt to Continue
+
+Ask if the user wants to calculate BMI for another person (1 to continue, 0 to terminate).
+Read user's choice.
+Decision
+IF choice == 0:
+Print "We finish, thank you".
+End the program.
+ELSE:
+Return to step 2.
+
+__End
+End the program
+
+
+
+
+
+
+  
