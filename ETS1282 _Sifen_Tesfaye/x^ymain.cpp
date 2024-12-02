d2f04ea707e1580cd36c398f4e8b8ec49ba9dcdf
Sifen tesfaye kumsa <sifen.tesfaye@aastustudent.edu.et>
2024-12-01 17:53:21 -0800

    Update x^ymain.cpp

--- ETS1282 _Sifen_Tesfaye/x^ymain.cpp
+++ ETS1282 _Sifen_Tesfaye/x^ymain.cpp
@@ -1,2 +1,21 @@
-#including<iostream>
-using nam
+/*Write a program that find the result of the expression 𝑥^𝑦 where the value of X and Y are entered by the user. */
+#include<iostream>
+using namespace std;
+
+int main () {
+    int x, y, product = 1;
+
+    cout << "Enter x: " << endl;
+    cin >> x;
+
+    cout << "Enter y: " << endl;
+    cin >> y;
+
+    for(int i = 1; i <= y; i++) {
+        product *= x;
+    }
+
+    cout << x << " the power of " << y << " = " << product << endl;
+
+    return 0;
+}
