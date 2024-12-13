28a4481235da2da1373ac7786a5cca261d3db543
sosnage12 <sosna.gemechu@aastustudent.edu.et>
2024-12-13 20:38:57 +0300

    Update question_4_b.cpp

--- ETS1313 _sosna_gemechu/Assignment_2/question_4_b.cpp
+++ ETS1313 _sosna_gemechu/Assignment_2/question_4_b.cpp
@@ -1,49 +1,98 @@
- #include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int row,colnums;
-    cout<<"enter the number of rows : "<<endl;
-    cin>>row;
-    cout<<"enter the number of colnums : "<<endl;
-    cin>>colnums;
-    for (int j = 1; j <=colnums; j++)
-    {
-        int z=5-j;
-        row=row-(row-j);
-        for(int k=1; k<=z;k++){
-        cout<<" ";
-        
+
+int main() {
+    int choice;
+    cout << "Choose a program to run:\n";
+    cout << "1. Print alphabet grid\n";
+    cout << "2. Print decreasing stars\n";
+    cout << "3. Print hollow rectangle\n";
+    cout << "4. Print inverted hollow triangle\n";
+    cout << "5. Print right-aligned triangle\n";
+    cout << "6. Print inverted right-aligned triangle\n";
+    cout << "7. Print hollow right-aligned triangle\n";
+    cout << "Enter your choice (1-7): ";
+    cin >> choice;
+
+    if (choice == 1) {
+        for (char i = 'a'; i <= 'e'; i++) {
+            for (char j = 'a'; j <= 'e'; j++) {
+                cout << j << " ";
+            }
+            cout << endl;
         }
-    for (int i = row; i >=1; i--)
-    {
-        cout<<"*"<<" ";
-        
+    } else if (choice == 2) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= (n - i); j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    } else if (choice == 3) {
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 1 || i == n || j == 0 || j == n) {
+                    cout << "* ";
+                } else {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
+    } else if (choice == 4) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= (n - i); j++) {
+                if (i == 1 || j == (n - i) || j == 0) {
+                    cout << "* ";
+                } else {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
+    } else if (choice == 5) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int k = n - i; k > 0; k--) {
+                cout << " ";
+            }
+            for (int j = 1; j <= i; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    } else if (choice == 6) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= i - 1; k++) {
+                cout << " ";
+            }
+            for (int j = i; j <= n; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    } else if (choice == 7) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int k = n - i; k > 0; k--) {
+                cout << " ";
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i == n || j == i || j == 1) {
+                    cout << "* ";
+                } else {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "Invalid choice. Please run the program again.\n";
     }
-    cout<<""<<endl;
-    }
-    return 0;}
-   */
-  /*
-  inverted full pyramid
-  #include <iostream>  
-using namespace std;  
 
-int main() {  
-    int rows;  
-    cout << "Enter the number of rows: " << endl;  
-    cin >> rows;  
-
-    for (int i = 0; i < rows; i++) {     
-        for (int j = 0; j < i; j++) {  
-            cout << " ";  
-        }  
-        for (int k = 0; k < rows - i; k++) {  
-            cout << "* ";  
-        }  
-        cout << endl;  
-    }  
-    
-    return 0;   
+    return 0;
 }
-
-  
