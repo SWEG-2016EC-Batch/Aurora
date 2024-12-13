0f1b31a99557715c84a0b15b3c907f9594a3ec38
sosnage12 <sosna.gemechu@aastustudent.edu.et>
2024-12-13 21:13:10 +0300

    Update question_4_a.cpp

--- ETS1313 _sosna_gemechu/Assignment_2/question_4_a.cpp
+++ ETS1313 _sosna_gemechu/Assignment_2/question_4_a.cpp
@@ -1,101 +1,181 @@
 #include <iostream>
 using namespace std;
-int main(){
-  
-    int choice;
-    cout << "Choose a program to run:\n";
-    cout << "1. Print numbers in rows and columns\n";
-    cout << "2. Print alphabet in rows\n";
-    cout << "3. Print decreasing numbers with row adjustments\n";
-    cout << "4. Print inverted numbers with spaces\n";
-    cout << "5. Print increasing alphabets row-wise\n";
-    cout << "6. Print lowercase alphabets in rows\n";
-    cout << "Enter your choice (1-6): ";
+
+int main() {
+    int choice, cont;
+do{
+    cout << "1: rectangle of numbers \n";
+    cout << "2: Rectangle of uppercase Alphabets \n";
+    cout << "3: Incremental number pyramid \n";
+    cout << "4: Reversed incremental number pyramid \n";
+    cout << "5: Alphabetic half pyramid \n";
+    cout << "6: Rectangle of lowercase Alphabets \n";
+    cout << "7: Hollow star rectangle \n";
+    cout << "8: Inverted star half pyramid \n";
+    cout << "9: Hollow inverted star pyramid \n";
+    cout << "10: Star pyramid \n";
+    cout << "11: Inverted star pyramid \n";
+    cout << "12: Hollow star pyramid \n";
+    cout << endl;
+    cout << "Enter the choice of pattern: ";
     cin >> choice;
 
-    if (choice == 1) {
-        int row, colnums;
-        cout << "Enter the number of rows: ";
-        cin >> row;
-        cout << "Enter the number of columns: ";
-        cin >> colnums;
-        for (int j = 1; j <= colnums; j++) {
-            for (int i = 1; i <= row; i++) {
-                cout << i;
-            }
-            cout << endl;
-        }
-    } 
-    else if (choice == 2) {
-        char chart = 'A';
-        for (int i = 1; i <= 24; i++) {
-            cout << chart;
-            chart++;
-            if (i % 6 == 0){
+
+    switch (choice) {
+        case 1:
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 1; j <= 5; j++) {
+                    cout << j << " ";
+                }
                 cout << endl;
-        }}}
-         else if (choice == 3) {
-        int row, colnums;
-        cout << "Enter the number of rows: ";
-        cin >> row;
-        cout << "Enter the number of columns: ";
-        cin >> colnums;
-        for (int j = 1; j <= colnums; j++) {
-            row = row - (row - j);
-            for (int i = 1; i <= row; i++) {
-                cout << i;
             }
-            cout << endl;
+            break;
+
+        case 2: {
+            char letter = 'A';
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 1; j <= 5; j++) {
+                    cout << letter << " ";
+                    if (letter == 'Z')
+                    {
+                        break;
+                    }
+                    letter++;
+                }
+                cout << endl;
+            }
+            break;
         }
-    } 
-    else if (choice == 4) {
-        int row, colnums;
-        cout << "Enter the number of rows: ";
-        cin >> row;
-        cout << "Enter the number of columns: ";
-        cin >> colnums;
-        for (int j = 1; j <= colnums; j++) {
-            int z = 5 - j;
-            row = row - (row - j);
-            for (int k = 1; k <= z; k++) {
-                cout << " ";
+
+        case 3:
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 1; j <= i; j++) {
+                    cout << j << " ";
+                }
+                cout << endl;
             }
-            for (int i = row; i >= 1; i--) {
-                cout << i;
+            break;
+
+        case 4:
+            for (int i = 1; i <= 5; i++) {
+                for (int k = 5-i; k >= 1; k--) {
+                    cout << "  ";
+                }
+                for (int j = i; j >= 1; j--) {
+                    cout << j << " ";
+                }
+                cout << endl;
             }
-            cout << endl;
-        }
-    } 
-    else if (choice == 5) {
-        int row, colnums;
-        cout << "Enter the number of rows: ";
-        cin >> row;
-        cout << "Enter the number of columns: ";
-        cin >> colnums;
-        char ch = 'A';
-        for (int j = 1; j <= colnums; j++) {
-            for (int i = 1; i <= j; i++) {
-                cout << ch << " ";
-                ++ch;
+            break;
+
+        case 5:
+            for (int i = 1; i <= 5; i++) {
+                char letter = 'A';
+                for (int j = 1; j <= i; j++) {
+                    cout << letter << " ";
+                    letter++;
+                }
+                cout << endl;
             }
-            cout << endl;
-            ch = 'A';
-        }
-    } 
-    else if (choice == 6) {
-        int row, colnums;
-        cout << "Enter the number of rows: ";
-        cin >> row;
-        cout << "Enter the number of columns: ";
-        cin >> colnums;
-        char ch = 'a';
-        for (int j = 1; j <= colnums; j++) {
-            for (int i = 1; i <= row; i++) {
-                cout << ch;
-                ++ch;
+            break;
+
+        case 6:
+            for (int i = 1; i <= 5; i++) {
+                char letter = 'a';
+                for (int j = 1; j <= 5; j++) {
+                    cout << letter << " ";
+                    letter++;
+                }
+                cout << endl;
             }
-            cout << endl;
-            ch = 'a';
-        }}
-  return 0;
+            break;
+case 7:
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 1; j <= 5; j++) {
+                    if (i == 1||i == 5 j == 1 ||j == 5) {
+                        cout << "* ";
+                    } else {
+                        cout << "  ";
+                    }
+                }
+                cout << endl;
+            }
+            break;
+
+        case 8:
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 5; j >= i; j--) {
+                    cout << "* ";
+                }
+                cout << endl;
+            }
+            break;
+
+        case 9:
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 5; j >= i; j--) {
+                    if (i == 1 ||j == 5  ||i == j) {
+                        cout << "* ";
+                    } else {
+                        cout << "  ";
+                    }
+                }
+                cout << endl;
+            }
+            break;
+
+        case 10:
+            for (int i = 1; i <= 5; i++) {
+                for (int k = 5 - i; k >= 1; k--) {
+                    cout << " ";
+                }
+                for (int j = 1; j <= i; j++) {
+                    cout << "* ";
+                }
+                cout << endl;
+            }
+            break;
+
+        case 11:
+            for (int i = 1; i <= 5; i++) {
+                for (int k = 1; k <= i; k++) {
+                    cout << " ";
+                }
+                for (int j = 5; j >= i; j--) {
+                    cout << "* ";
+                }
+                cout << endl;
+            }
+            break;
+
+        case 12:
+            for (int i = 1; i <= 5; i++) {
+                for (int k = 5 - i; k >= 1; k--) {
+                    cout << " ";
+                }
+                for (int j = 1; j <= i; j++) {
+                    if (i == 1  ||j == 1  ||j == i  i == 5) {
+                        cout << "* ";
+                    } else {
+                        cout << "  ";
+                    }
+                }
+                cout << endl;
+            }
+            break;
+
+        default:
+            cout << "Invalid choice" << endl;
+            break;
     }
+cout << endl;
+cout << "Press 1 to continue 0 to exit ";
+cin >> cont;
+cout <<endl;
+
+} while(cont == 1);
+cout << endl;
+cout << "Thank you !!!";
+
+    return 0;
+}
