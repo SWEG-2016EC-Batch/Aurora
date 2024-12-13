a1005c51379c5ad54450306f4d21110977ceffc9
Semehal Hailu Woldeslassie <semehal.hailu@aastustudent.edu.et>
2024-12-13 14:55:32 +0300

    Create Question_4a(3).cpp

---
+++ ETS1266_Semehal_Hailu/Assingment_2/Question_4a(3).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int row,colnums;
+    cout<<"enter the number of rows : "<<endl;
+    cin>>row;
+    cout<<"enter the number of colnums : "<<endl;
+    cin>>colnums;
+    for (int j = 1; j <=colnums; j++)
+    {
+        row=row-(row-j);
+    for (int i = 1; i <=row; i++)
+    {
+        cout<<i<<" ";
+        
+    }
+    cout<<""<<endl;
+    
+    }
+    return 0;
+}
