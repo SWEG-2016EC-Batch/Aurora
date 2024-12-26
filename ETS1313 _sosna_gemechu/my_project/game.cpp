90ca5790885093748af2d43340d3720a51482e90
sosnage12 <sosna.gemechu@aastustudent.edu.et>
2024-12-26 15:11:44 +0300

    Update game.cpp

--- ETS1313 _sosna_gemechu/my_project/game.cpp
+++ ETS1313 _sosna_gemechu/my_project/game.cpp
@@ -5,9 +5,17 @@ int main() {
     bool game=true;
     char X,O,o,x;
     cout<<" WELCOME TO TIC TAK TOE GAME\n";
-    int num=1;
-    cout<<"if you want to start click one\n";
-    cin>>num; cout<<"\n";
+    int num;
+    cout<<"are you ready to start ??\n";
+    cout<<" Yes! click 1\n";
+    cin>>num; 
+    if(num==1){
+goto line0;
+    } else {
+        cout<<"invalid input!!\n";
+    }
+    line0  :
+     cout<<"\n";
     char board [3][3]={{'*','*','*'},{'*','*','*'},{'*','*','*'}};
     for(int i=0;i<=2;i++){
         cout<<"-------------\n";
@@ -20,8 +28,13 @@ int main() {
     while(game){
     for(int n=1;n<=5;n++){
     int row, column;
+    line1 :
     cout<<"Player X ente row and column you want to pick star\n";
 cin>>row>>column;
+if(row>3||column>3){
+    cout<<"invalid in put please enter valid in put\n";
+ goto line1;
+}
 row--;
 column--;
  for(int k=0;k<=2;k++){
@@ -39,8 +52,13 @@ board[k][m]='X';
         cout<<"|"<<endl;
     }
     cout<<"-------------\n";
+    line2 :
      cout<<"Player O ente row and column you want to pick star\n";
 cin>>row>>column;
+if(row>3||column>3){
+    cout<<"invalid in put please enter valid in put\n";
+    goto line2;
+}
 row--;
 column--;
  for(int a=0;a<=2;a++){
@@ -57,14 +75,16 @@ board[a][s]='O';
         cout<<"|"<<endl;
     }
     cout<<"-------------\n";
-    for(int r=1;r<=2;r++){
-            if(board[r][0]=='X'&&board[r][1]=='X'&&board[r][2]=='X'){
- cout<<" dear X youu win\n";
- break;
-        } else if(board[r][0]=='O'&&board[r][1]=='O'&&board[r][2]=='O'){
+    for(int r=0;r<=2;r++){
+            if(board[r][0]=='O'&&board[r][1]=='O'&&board[r][2]=='O'||board[0][r]=='O'&&board[1][r]=='O'&&board[2][r]=='O'){
     cout<<" dear O youu win\n";
-    break;
-    }
+    } else  if(board[r][0]=='X'&&board[r][1]=='X'&&board[r][2]=='X'||board[0][r]=='X'&&board[1][r]=='X'&&board[2][r]=='X') {
+ cout<<" dear X youu win\n";
+        } else if(board[0][0]=='X'&&board[1][2]=='X'&&board[3][3]=='X'){
+            cout<<"dear X you win\n";
+        } else if(board[0][0]=='O'&&board[1][2]=='O'&&board[3][3]=='O'){
+            cout<<"dear O you win\n";
+        } 
     }
     }
     }
