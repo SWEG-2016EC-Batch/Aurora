c0e8e5245507f895b432d948c4cb4e0adbc417fb
Sifen tesfaye kumsa <sifen.tesfaye@aastustudent.edu.et>
2025-01-02 23:00:58 -0800

    Update game.cpp

--- ETS1313 _sosna_gemechu/my_project/game.cpp
+++ ETS1313 _sosna_gemechu/my_project/game.cpp
@@ -29,3 +29,37 @@ b:
         cout << "Invalid input! Please enter 1 to start.\n";  
         goto b;  
     }
+int num2;  
+    // Ask the user to choose the game mode  
+    cout << "Do you want to play with your friend or with me??\n";  
+a:  
+    cout << "If you want to play with me press 1,\n if you want to play with your friend press 0\n";  
+    cin >> num2;  
+    while (cin.fail() || (num2 != 1 && num2 != 0)) {  
+        cin.clear();  
+        cin.ignore();  
+        cout << "Invalid input! Please enter 1 to play with me or 0 to play with your friend.\n";  
+        goto a;  
+    }  
+
+    if (num2 == 1) {  
+        chooseplayer:  
+        // Allow player to choose their marker (X or O)  
+        cout << "Which player would you like to start as? (X/O): ";  
+        cin >> currentPlayer;  
+        if (currentPlayer != 'X' && currentPlayer != 'O') {  
+            cout << "Invalid choice! Please choose either X or O.\n";  
+            goto chooseplayer;  
+        }  
+
+        // Initialize the game board  
+        char board[3][3] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
+
+
+
+
+
+
+
+
+
