80b2f6e5751a7140e390dba33af1c153cbf33347
soliyana wasiyhun <soliyana.wasiyhun@aastustudent.edu.et>
2025-01-02 22:22:22 -0800

    Update game.cpp

--- ETS1313 _sosna_gemechu/my_project/game.cpp
+++ ETS1313 _sosna_gemechu/my_project/game.cpp
@@ -1,1 +1,31 @@
+#include <iostream>  
+#include <iomanip>  
+#include <cstdlib>  
+#include <ctime>  
+#include <windows.h>  
+using namespace std;  
 
+int main() {  
+    // Tracks the current player ('X' or 'O') and computer marker  
+    char currentPlayer;  
+    char computer;  
+
+    // Display welcome message  
+    cout << "WELCOME TO TIC TAC TOE GAME\n";  
+    int num;  
+
+startgame:  
+    // Prompt user to start the game  
+    cout << "Are you ready to start??\n";  
+    cout << "Yes! Click 1\n";  
+    
+b:  
+    cin >> num;  
+
+    // Validate the start input  
+    while (cin.fail() || num != 1) {  
+        cin.clear(); // Clear input error  
+        cin.ignore(); // Discard invalid input  
+        cout << "Invalid input! Please enter 1 to start.\n";  
+        goto b;  
+    }
